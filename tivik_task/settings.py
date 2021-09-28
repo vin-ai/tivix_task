09d55dd7720d488e46594db2cf35a32fbd0bf822
Vin.AI <vinaykrsharma1986@gmail.com>
2021-09-29 03:12:08 +0530

    Upd: Serve static content in dev mode
    - Congigyred Django static serve with ManifestStorage to serve revisioned file
    - Configured **urls.py** to serve static files on dev mode only

--- tivik_task/settings.py
+++ tivik_task/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_collected/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
