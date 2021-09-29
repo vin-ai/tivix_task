373c2ea319f719b9ff521f39959ff2a3526cb66a
VINAY KUMAR SHARMA <vinaykrsharma1986@gmail.com>
2021-09-29 16:34:42 +0530

    Fix: Spelling mistake on project name

--- tivix/wsgi.py
+++ tivix/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tivik_task project.
+WSGI config for tivix project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tivik_task.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tivix.settings')
 
 application = get_wsgi_application()
